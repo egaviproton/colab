4b3a8e77587c3f1239a8f2a23a43280806930062
egaviproton <egavi@protonmail.com>
2019-07-21 15:38:41 +0700

    added  grid_display_images

--- gavi_helper.py
+++ gavi_helper.py
@@ -52,5 +52,24 @@ def hms_string(sec_elapsed):
     return f"{h}:{m:>02}:{s:>05.2f}"
     
     
+import matplotlib.pyplot as plt
+
+def grid_display_images(list_of_images, list_of_titles=[], no_of_columns=5, figsize=(20,20)):
+  # tested with   
+  # from PIL import Image
+  fig = plt.figure(figsize = figsize)
+  column = 0
+  for i in range(len(list_of_images)):
+      column += 1
+      #  check for end of column and create a new figure
+      if column == no_of_columns+1:
+          fig = plt.figure(figsize = figsize)
+          column = 1
+      fig.add_subplot(1, no_of_columns, column)
+      plt.imshow(list_of_images[i])
+      plt.axis('off')
+      if len(list_of_titles) >= len(list_of_images):
+          plt.title(list_of_titles[i])
+    
 if __name__ == '__main__':
     show_version()
